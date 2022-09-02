21716f12a857c78a285e74e98b8f9849d61bb828
Brian <Bhensley811@gmail.com>
2022-09-01 22:44:44 -0400

    commit tree b8402acf0f7babab7d9f03b8ced118abf4f66b1d

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = (1)
+const num2 = (62)
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = Math.floor(Math.random() + 1)
+
+const num3 = 4
+const num4 = 1
+const mod = (num3 / num4)
+
+const max = Math.max(1,20)
